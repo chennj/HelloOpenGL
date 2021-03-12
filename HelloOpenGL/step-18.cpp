6128d663e14a03c9ab4e9e77843a3ae8698c395a
chennj <chennj_cn@aliyun.com>
2021-03-12 17:36:52 +0800

    Model View Projection Matrices in OpenGL

--- HelloOpenGL/step-18.cpp
+++ HelloOpenGL/step-18.cpp
@@ -1,20 +1,27 @@
 /**
 *
-* TOPIC: Projection Matrices in OpenGL
+* TOPIC: Model View Projection Matrices in OpenGL
 *
 * -- OpenGL 用到的数学库地址：https://github.com/g-truc/glm
 * -- orthod projection		正交投影一般用于2d
 * -- perspective projection 透视投影一般用于3d
-* -- normalized device coordinate (space)	
+* -- normalized device coordinate (space)
 *	 标准化设备坐标(空间)/规范化设备坐标(空间)
 *	 上下左右都是 [-1,1]
 * -- MVP = model view projection (matrix)	模型视图投影（矩阵）
+*	 实际上是三个矩阵，三种变换
+*	 模型变换(矩阵) -> 视图变换(矩阵) -> 投影变换(矩阵)
+*	 模型变换：被观察物体自身的旋转、缩放、错切、平移变换
+*	 视图变换：变换观察者的位置，在场景中放置照相机并让它指向某个方向
+*	 投影变换：投影变换将在模型变换->视图变换之后应用到顶点上，它将指定一个完成的场景
+	（所有视图模型变换都已完成）是如何投影到屏幕上的最终图像。
+	 即将物体的顶点坐标转换进到标准化设备坐标空间，即-1到1之间。
 *
 * 注意 -- basic expand 和 -- basic的顺序不要颠倒
 * glewInit() 必须在 glfwMakeContextCurrent(window) 后定义
 *
 */
-#ifdef __RUN__
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -41,7 +48,7 @@ int main(void)
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(960, 540, "Hello World", NULL, NULL);
 	if (!window)
 	{
 		glfwTerminate();
@@ -71,27 +78,15 @@ int main(void)
 	//也就是说像step-09中那样使用是不会出现这种情况的。
 	{
 		/**
-		*
-		* 如果将下面的positions[]的值放大8倍，结果图形将只显示中间的一部分,并且图形也放大了8倍
-		* -- 为什么呢？
-		*	 因为下面建立的正交投影 “glm::mat4 proj” 
-		*	 它的视口：左右宽度是[-2,2] = 4 上下高度是 [-1.5,1.5] = 3 
-		*	 在这个范围之外的图形都将不显示
+		* 将传递给Basic.shader的gl_Position
 		*/
 		float positions[] = {
-			-0.5f, -0.5f,/*左下角顶点坐标*/ 0.0f, 0.0f,	/*对应的纹理坐标*/
-			 0.5f, -0.5f,/*右下角顶点坐标*/ 1.0f, 0.0f,	/*对应的纹理坐标*/
-			 0.5f,  0.5f,/*右上角顶点坐标*/ 1.0f, 1.0f,	/*对应的纹理坐标*/
+			 100.0f,	100.0f,	/*左下角顶点坐标*/	0.0f, 0.0f,		/*对应的纹理坐标*/
+			 200.0f,	100.0f,	/*右下角顶点坐标*/	1.0f, 0.0f,		/*对应的纹理坐标*/
+			 200.0f,	200.0f,	/*右上角顶点坐标*/	1.0f, 1.0f,		/*对应的纹理坐标*/
 
-			-0.5f,  0.5f,/*左上角顶点坐标*/ 0.0f, 1.0f		/*对应的纹理坐标*/
+			 100.0f,	200.0f,	/*左上角顶点坐标*/	0.0f, 1.0f		/*对应的纹理坐标*/
 		};
-		//float positions[] = {
-		//	-4.0f, -4.0f,/*左下角顶点坐标*/ 0.0f, 0.0f,	/*对应的纹理坐标*/
-		//	 4.0f, -4.0f,/*右下角顶点坐标*/ 1.0f, 0.0f,	/*对应的纹理坐标*/
-		//	 4.0f,  4.0f,/*右上角顶点坐标*/ 1.0f, 1.0f,	/*对应的纹理坐标*/
-
-		//	-4.0f,  4.0f,/*左上角顶点坐标*/ 0.0f, 1.0f		/*对应的纹理坐标*/
-		//};
 
 		unsigned int indices[] = {
 			0, 1, 2,		//第一个三角形的三个顶点的索引（index）
@@ -126,24 +121,13 @@ int main(void)
 		*	 如果我有一个-0.5的顶点，它是向最左边的一个方向的四分之一，因为0.5是2的四分之一，
 		*	 所以，那么这个正交矩阵将这个位置转换成-1和1之间，那么0到-1之间的四分之一是0.25
 		*/
-		glm::mat4 proj = glm::ortho(-0.5f, 3.5f, -0.5f, 2.5f, -1.0f, 1.0f);	// 纵横比 = 3：4 远近比 1：1
-		//glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);	// 纵横比 = 3：4 远近比 1：1
-		//使用这一组坐标看看图形有什么变化--结果图形会缩小一倍
-		//glm::mat4 proj = glm::ortho(-4.0f, 4.0f, -3.0f, 3.0f, -1.0f, 1.0f);	
-		/**
-		* 测试正交投影矩阵的作用
-		* 在result_test加上断点去看看vp_test经过正交矩阵proj_test的之后
-		* result_test的(x,y)变成了多少
-		* 结果是(-1.0,-0.3] 即左上角的坐标被映射到了屏幕的左下部,也就是演示图形所在的位置。
-		*/
-		//proj_test就是我们将要传递给shader的正交投影矩阵proj
-		glm::mat4 proj_test = glm::ortho(-0.5f, 3.5f, -0.5f, 2.5f, -1.0f, 1.0f);
-		//vp_test是positions[]左上角顶点坐标
-		glm::vec4 vp_test(-0.5f, 0.5f, 0.0f, 1.0f);
-		//正交投影矩阵proj_test将要把顶点坐标vp_test转换到[-1,1]的标准化设备坐标空间
-		//它的结果就是Basic.shader中的gl_Position = u_MVP * position，只不过是其中一个顶点的的转换结果
-		//结果就是左上角的顶点移动到了屏幕的左下部，也就是整个图形显式在了视口的左下角。
-		glm::vec4 result_test = proj_test * vp_test;
+		glm::mat4 proj	= glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);	// 纵横比 = 9：16 保持和窗口相同的纵横比 远近比 1：1
+		// 模型变换矩阵：作用是物体像右上移动200
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+		// 视图变换矩阵：作用是物体向左移动，相当于相机向右移动。
+		glm::mat4 view	= glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+		// mvp等于先投影然后右上移动200然后向左平移100
+		glm::mat4 mvp = proj * model * view;
 
 		//shader																																		
 		Shader shader("Basic.shader");
@@ -152,7 +136,7 @@ int main(void)
 		//使用uniform
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
 		//使用正交投影矩阵
-		shader.SetUniformMat4f("u_MVP", proj);
+		shader.SetUniformMat4f("u_MVP", mvp);
 
 		//纹理
 		Texture texture("../res/texture/texture-01.png");
@@ -201,4 +185,3 @@ int main(void)
 	glfwTerminate();
 	return 0;
 }
-#endif
